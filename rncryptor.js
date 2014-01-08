5261612eb2bb3cba3f42765ed1a104905d3d6653
Rob Napier <robnapier@gmail.com>
2014-01-08 11:04:38 -0500

    Basic test of PBKDF2. Way too slow to use.

---
+++ rncryptor.js
@@ -0,0 +1,8 @@
+var RNCryptor = {};
+
+/*
+    Takes password string and salt WordArray
+*/
+RNCryptor.KeyForPassword = function(password, salt) {
+    return CryptoJS.PBKDF2(password, salt, { keySize: 256/32, iterations: 10000 });
+}
--- tests/rncryptor-test.js
+++ tests/rncryptor-test.js
@@ -1,3 +1,5 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+test("KDF: One byte", function() {
+  var key = RNCryptor.KeyForPassword("a", CryptoJS.enc.Hex.parse("0102030405060708"));
+  equal(key.toString(), "fc632b0c a6b23eff 9a9dc3e0 e585167f 5a328916 ed19f835 58be3ba9 828797cd".replace(/\s/g,''));
 });
+
