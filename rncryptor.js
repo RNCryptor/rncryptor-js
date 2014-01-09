6f42f92812a41cf436bb209a6917f6c417709e1a
Rob Napier <robnapier@gmail.com>
2014-01-09 09:57:12 -0500

    Pass first KDF test

--- rncryptor.js
+++ rncryptor.js
@@ -4,5 +4,5 @@ var RNCryptor = {};
     Takes password string and salt WordArray
 */
 RNCryptor.KeyForPassword = function(password, salt) {
-    return CryptoJS.PBKDF2(password, salt, { keySize: 256/32, iterations: 10000 });
+    return CryptoJS.PBKDF2(password, salt, { keySize: 256/32, iterations: 1000 });
 }
--- tests/rncryptor-test.js
+++ tests/rncryptor-test.js
@@ -1,5 +1,5 @@
 test("KDF: One byte", function() {
   var key = RNCryptor.KeyForPassword("a", CryptoJS.enc.Hex.parse("0102030405060708"));
-  equal(key.toString(), "fc632b0c a6b23eff 9a9dc3e0 e585167f 5a328916 ed19f835 58be3ba9 828797cd".replace(/\s/g,''));
+  equal(key.toString(), "d48f10b7 ae39bd25 2bb68e1f af12acea 3474d7d7 702a15b2 ede3246e 82dbb2fd".replace(/\s/g,''));
 });
 
