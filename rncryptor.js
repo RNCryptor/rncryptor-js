3f09884dd7b5534e0ad724a0d7155aa155d488a3
Rob Napier <robnapier@gmail.com>
2014-01-10 14:14:46 -0500

    Pass all short-password tests

--- rncryptor.js
+++ rncryptor.js
@@ -2,7 +2,45 @@ var RNCryptor = {};
 
 /*
     Takes password string and salt WordArray
+    Returns key WordArray
 */
 RNCryptor.KeyForPassword = function(password, salt) {
     return CryptoJS.PBKDF2(password, salt, { keySize: 256/32, iterations: 1000 });
+}
+
+/*
+  Takes password string and plaintext WordArray
+  options:
+    iv
+    encryption_salt
+    html_salt
+  Returns ciphertext WordArray
+*/
+RNCryptor.Encrypt = function(password, plaintext, options) {
+  options = options || {}
+  var encryption_salt = options["encryption_salt"] || CryptoJS.lib.WordArray.random(64/8);
+  var encryption_key = RNCryptor.KeyForPassword(password, encryption_salt);
+
+  var hmac_salt = options["hmac_salt"] || CryptoJS.lib.WordArray.random(64/8)
+  var hmac_key = RNCryptor.KeyForPassword(password, hmac_salt);
+
+  var iv = options["iv"] || CryptoJS.lib.WordArray.random(64/8)
+
+  var version = CryptoJS.enc.Hex.parse("03");
+  var options = CryptoJS.enc.Hex.parse("01");
+  
+  var message = version.clone();
+  message.concat(options);
+  message.concat(encryption_salt);
+  message.concat(hmac_salt);
+  message.concat(iv);
+
+  var encrypted = CryptoJS.AES.encrypt(plaintext, encryption_key, {iv: iv});
+  message.concat(encrypted.ciphertext);
+
+  var hmac = CryptoJS.HmacSHA256(message, hmac_key);
+
+  message.concat(hmac);
+
+  return message;
 }
--- tests/rncryptor-test.js
+++ tests/rncryptor-test.js
@@ -2,3 +2,14 @@ var verify_kdf_short = function(vector) {
   var key = RNCryptor.KeyForPassword(vector["password"], CryptoJS.enc.Hex.parse(vector["salt_hex"]));
   equal(key.toString(), vector["key_hex"].replace(/\s/g,''));
 }
+
+var verify_password_short = function(vector) {
+  var ciphertext = RNCryptor.Encrypt(vector["password"], 
+                                     CryptoJS.enc.Hex.parse(vector["plaintext_hex"].replace(/\s/g,'')), 
+                                     { "encryption_salt": CryptoJS.enc.Hex.parse(vector["enc_salt_hex"].replace(/\s/g,'')),
+                                       "hmac_salt": CryptoJS.enc.Hex.parse(vector["hmac_salt_hex"].replace(/\s/g,'')),
+                                       "iv": CryptoJS.enc.Hex.parse(vector["iv_hex"].replace(/\s/g,''))
+                                     });
+
+  equal(ciphertext.toString(), vector["ciphertext_hex"].replace(/\s/g,''));
+}
