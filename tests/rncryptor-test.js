a0a06086600f4fec88d986efa71e462803e3d1fe
Rob Napier <robnapier@gmail.com>
2014-01-10 11:14:31 -0500

    KDF (short) vectors pass

--- tests/rncryptor-test.js
+++ tests/rncryptor-test.js
@@ -1,5 +1,4 @@
-test("KDF: One byte", function() {
-  var key = RNCryptor.KeyForPassword("a", CryptoJS.enc.Hex.parse("0102030405060708"));
-  equal(key.toString(), "d48f10b7 ae39bd25 2bb68e1f af12acea 3474d7d7 702a15b2 ede3246e 82dbb2fd".replace(/\s/g,''));
-});
-
+var verify_kdf_short = function(vector) {
+  var key = RNCryptor.KeyForPassword(vector["password"], CryptoJS.enc.Hex.parse(vector["salt_hex"]));
+  equal(key.toString(), vector["key_hex"].replace(/\s/g,''));
+}
