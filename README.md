6a5fc0eb962fbf183b42056e73aa450797193815
Alexey Sh <alexey.sunshine@gmail.com>
2016-03-14 00:32:44 +0600

    refactored but removed tests

--- README.md
+++ README.md
@@ -3,11 +3,52 @@ rncryptor-js
 
 Rough JavaScript implementation of [RNCryptor](https://github.com/RNCryptor/RNCryptor).
 
-It is very possible that this is too slow for you. Please let me know what kinds of performance you're seeing.
+## Example usage
 
-You must install sjcl as well as rncryptor-js. Note that plaintext and ciphertext are sjcl.bitArrays, not as strings. It is currently up to you to convert.
+```javascript
+var settings = {
+    hmac_salt: "FFGE/kyU1TY=",
+    salt: "rfAaaGiNBFM=",
+    password: 'spring'
+};
 
-BTW: This project would love an owner. I hate JavaScript. But I love secure systems. There is too much JavaScript out there not to provide an easy-to-use crypto framework that interoperates well with other platforms. So I started working on it. But if you're good a JavaScript and would like to work on this, I can talk you through the crypto. As you can see from this implementation, the code isn't actually very complicated since it relies on SJCL for the all the heavy lifting.
+
+var cryptor = new RNCryptor({
+    HMAC_SALT: RNCryptor.helpers.base64ToHex(settings.hmac_salt),
+    ENCRYPTION_SALT: RNCryptor.helpers.base64ToHex(settings.salt),
+    PASSWORD: settings.password
+});
+
+encrypted_msg = cryptor.encrypt('top secret');
+
+decrypted_msg = cryptor.decrypt(encrypted_msg); // => top secret
+
+```
+
+or you can setup RNCryptor without master password, via keys
+```javascript
+cryptor = new RNCryptor({
+    KEY: '8872312fbc7e985943e079f1836220b55d22da3101c5d8328d17cf6eb4637469',
+    HMAC_KEY: '0871e2b30fa7ff09b329560f0e4729e75611369fedac762c78332d0825cc676c',
+    HMAC_SALT: RNCryptor.helpers.base64ToHex(settings.hmac_salt),
+    ENCRYPTION_SALT: RNCryptor.helpers.base64ToHex(settings.salt)
+});
+
+```
+
+
+The encrypted data contains:
+
+version + options + salt + hmacSalt + iv + cipher + hmac
+
+- version - version of the encryption logic
+- options - never used
+- salt - encryption salt, by which the encryption key is generated
+- hmacSalt - salt by which the hmac key is generated
+- iv - will used in aes
+- cipher - encrypted data, the result of aes encryption
+- hmac - hash(version + options + salt + hmacSalt + iv + cipher), used for checking that data isn't corrupted
+
 
 SJCL (1.0.0) config:
 ./configure --without-all --with-aes --with-bitArray --with-codecHex --with-sha256 --with-sha1 --with-hmac --with-pbkdf2 --with-random --with-convenience --with-cbc
--- rncryptor.js
+++ rncryptor.js
@@ -1,96 +1,260 @@
-var RNCryptor = {};
+"use strict";
 
-/*
-    Takes password string and salt WordArray
-    Returns key bitArray
-*/
+(function (window) {
 
-RNCryptor.KeyForPassword = function(password, salt) {
-  var hmacSHA1 = function (key) {
-      var hasher = new sjcl.misc.hmac(key, sjcl.hash.sha1);
-      this.encrypt = function () {
-          return hasher.encrypt.apply(hasher, arguments);
-      };
-  };
-  return sjcl.misc.pbkdf2(password, salt, 10000, 32 * 8, hmacSHA1);
-}
+    window.RNCryptor = RNCryptor;
 
-/*
-  Takes password string and plaintext bitArray
-  options:
-    iv
-    encryption_salt
-    html_salt
-  Returns ciphertext bitArray
-*/
-RNCryptor.Encrypt = function(password, plaintext, options) {
-  options = options || {}
-  var encryption_salt = options["encryption_salt"] || sjcl.random.randomWords(8 / 4); // FIXME: Need to seed PRNG
-  var encryption_key = RNCryptor.KeyForPassword(password, encryption_salt);
+    var defaultSettings, helpers;
 
-  var hmac_salt = options["hmac_salt"] || sjcl.random.randomWords(8 / 4);
-  var hmac_key = RNCryptor.KeyForPassword(password, hmac_salt);
+    defaultSettings = {
+        PBKDF_ITERATIONS: 10000,
+        ENCRYPTION_SALT: null,
+        KEY: null,
+        PASSWORD: null,
+        PBKDF2_SIZE: 32,
+        HMAC_SALT_LENGTH: 8,
+        ENCRYPTION_SALT_LENGTH: 8,
+        IV_LENGTH: 16,
+        VERSION_LENGTH: 1,
+        OPTIONS_LENGTH: 1,
+        AES_MODE: 'cbc',
+        HMAC_SALT: null
+    };
 
-  var iv = options["iv"] || sjcl.random.randomWords(16 / 4);
+    helpers = {
+        stringToBase64: function (str) {
+            return sjcl.codec.base64.fromBits(sjcl.codec.utf8String.toBits(str));
+        },
+        base64ToString: function (str) {
+            return sjcl.codec.utf8String.fromBits(sjcl.codec.base64.toBits(str));
+        },
+        generateHMACSHA256: function (keyHex, msgHex) {
+            var hmac, keyBit, msgBit, resultBit;
+            keyBit = sjcl.codec.hex.toBits(keyHex);
+            msgBit = sjcl.codec.hex.toBits(msgHex);
+            hmac = new sjcl.misc.hmac(keyBit);
+            resultBit = hmac.encrypt(msgBit);
+            return sjcl.codec.hex.fromBits(resultBit);
+        },
+        generatePBKDF2WithSHA1: function (passwordText, saltHex, iter, keyLength) {
+            var hmacSHA1, pbkdf2, saltBits;
+            if (keyLength == null) {
+                keyLength = 256;
+            }
+            hmacSHA1 = function (key) {
+                var hasher;
+                hasher = new sjcl.misc.hmac(key, sjcl.hash.sha1);
+                this.encrypt = function () {
+                    return hasher.encrypt.apply(hasher, arguments);
+                };
+            };
+            saltBits = sjcl.codec.hex.toBits(saltHex);
+            pbkdf2 = sjcl.misc.pbkdf2(passwordText, saltBits, iter, keyLength, hmacSHA1);
+            return sjcl.codec.hex.fromBits(pbkdf2);
+        },
+        getRandomHex: function (len) {
+            return sjcl.codec.hex.fromBits(sjcl.random.randomWords((len / 8) * 2));
+        },
+        hexTobin: function (hex) {
+            var i, str;
+            str = '';
+            i = 0;
+            while (i < hex.length) {
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+                i += 2;
+            }
+            return str;
+        },
+        stringToHex: function (str) {
+            return sjcl.codec.hex.fromBits(sjcl.codec.utf8String.toBits(str));
+        },
+        hexToString: function (str) {
+            return sjcl.codec.utf8String.fromBits(sjcl.codec.hex.toBits(str));
+        },
+        hexToBase64: function (hex) {
+            return sjcl.codec.base64.fromBits(sjcl.codec.hex.toBits(hex));
+        },
+        base64ToHex: function (strInBase64) {
+            return sjcl.codec.hex.fromBits(sjcl.codec.base64.toBits(strInBase64));
+        },
+        binToHex: function (binData) {
+            return this.base64ToHex(this.btoa(binData));
+        },
+        chr: function (codePt) {
+            return String.fromCharCode(codePt);
+        },
+        btoa: function (data) {
+            return btoa(data);
+        },
+        atob: function (data) {
+            return atob(data);
+        },
+        trim: function (str) {
+            return str.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, '').replace(/\s+/g, ' ');
+        },
+        cleanString: function (str) {
+            return this.trim(str).replace(/\s+/g, '').replace(/(\r\n|\n|\r)/g, '');
+        },
+        extend: function (dest, source) {
+            for (var i in source) {
+                if (source.hasOwnProperty(i)) {
+                    dest[i] = source[i];
+                }
+            }
+            return dest;
+        },
+        sjcl: sjcl
+    };
 
-  var version = sjcl.codec.hex.toBits("03");
-  var options = sjcl.codec.hex.toBits("01");
-  
-  var message = sjcl.bitArray.concat(version, options);
-  message = sjcl.bitArray.concat(message, encryption_salt);
-  message = sjcl.bitArray.concat(message, hmac_salt);
-  message = sjcl.bitArray.concat(message, iv);
+    function CryptoError(message, data) {
+        var tmp = Error.apply(this, arguments);
+        tmp.name = this.name = 'RNCryptor error!';
+        data = data ? JSON.stringify(data) : '';
+        this.message = (tmp.message || '') + '\n' + data;
+        Object.defineProperty(this, 'stack', {
+            get: function () {
+                return tmp.stack
+            }
+        });
+        return this
+    }
 
-  var aes = new sjcl.cipher.aes(encryption_key);
-  sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
-  var encrypted = sjcl.mode.cbc.encrypt(aes, plaintext, iv);
+    var IntermediateInheritor = function () {
+    };
+    IntermediateInheritor.prototype = Error.prototype;
+    CryptoError.prototype = new IntermediateInheritor();
 
-  message = sjcl.bitArray.concat(message, encrypted);
 
-  var hmac = new sjcl.misc.hmac(hmac_key).encrypt(message);
-  message = sjcl.bitArray.concat(message, hmac);
+    RNCryptor.helpers = helpers;
 
-  return message;
-}
+    RNCryptor.CryptoError = CryptoError;
 
-/*
-  Takes password string and message (ciphertext) bitArray
-  options:
-    iv
-    encryption_salt
-    html_salt
-  Returns plaintext bitArray
-*/
-RNCryptor.Decrypt = function(password, message, options) {
-  options = options || {}
+    /*
+     @param params {Object}
+     @param params.ENCRYPTION_SALT {String} Salt in hex
+     @param params.HMAC_SALT {String}  Salt for HMAC
+     @param params.HMAC_KEY [String] Encryption key. Or provide a PASSWORD
+     @param params.KEY [String] Encryption key. Or provide a PASSWORD
+     @param params.PASSWORD [String] Master password. Or provide a KEY
 
-  var version = sjcl.bitArray.extract(message, 0 * 8, 8);
-  var options = sjcl.bitArray.extract(message, 1 * 8, 8);
+     @param params.PBKDF_ITERATIONS [Number]
+     @param params.AES_MODE [String] See available mode in sjcl lib
+     @param params.PBKDF2_SIZE [Number] Length in bytes.
+     @param params.HMAC_SALT_LENGTH [Number]  Length in bytes.
+     @param params.ENCRYPTION_SALT_LENGTH [Number] Length in bytes.
+     @param params.IV_LENGTH [Number] Length in bytes.
+     @param params.VERSION_LENGTH [Number] Length in bytes.
+     @param params.OPTIONS_LENGTH [Number] Length in bytes.
+     */
 
-  var encryption_salt = sjcl.bitArray.bitSlice(message, 2 * 8, 10 * 8);
-  var encryption_key = RNCryptor.KeyForPassword(password, encryption_salt);
+    function RNCryptor(params) {
+        var that = this;
+        params = helpers.extend(helpers.extend({}, defaultSettings), params);
+        sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
+        if (!params.HMAC_KEY && typeof params.PASSWORD === 'string') {
+            params.HMAC_KEY = helpers.generatePBKDF2WithSHA1(params.PASSWORD, params.HMAC_SALT, params.PBKDF_ITERATIONS);
+        }
+        if (!params.KEY && typeof params.PASSWORD === 'string') {
+            params.KEY = helpers.generatePBKDF2WithSHA1(params.PASSWORD, params.ENCRYPTION_SALT, params.PBKDF_ITERATIONS);
+        }
+        params.options = helpers.chr(1);
+        params.version = helpers.chr(2);
+        this.settings = helpers.extend(helpers.extend({}, defaultSettings), params);
+        ['KEY', 'HMAC_KEY', 'HMAC_SALT', 'ENCRYPTION_SALT'].forEach(function (key) {
+            if (!that.settings[key]) {
+                throw new CryptoError(key + " is not defined");
+            }
+        });
+    }
 
-  var hmac_salt = sjcl.bitArray.bitSlice(message, 10 * 8, 18 * 8);
-  var hmac_key = RNCryptor.KeyForPassword(password, hmac_salt);
+    /**
+     * @param {String} message
+     * @return {String} Base 64 encoded encrypted message
+     * */
+    RNCryptor.prototype.encrypt = function (message) {
+        var e, encrypted, encryptedBin, encryptedHex, encryptedMessage, encryptionSaltBin, hMacSaltBin, hmacHash,
+            ivBin, ivBit, ivHex, keyBit, messageBit, msg, options, prp, version;
+        version = this.settings.version;
+        options = this.settings.options;
+        encryptionSaltBin = helpers.hexTobin(this.settings.ENCRYPTION_SALT);
+        hMacSaltBin = helpers.hexTobin(this.settings.HMAC_SALT);
+        ivHex = helpers.getRandomHex(this.settings.IV_LENGTH);
+        keyBit = sjcl.codec.hex.toBits(this.settings.KEY);
+        ivBit = sjcl.codec.hex.toBits(ivHex);
+        ivBin = helpers.hexTobin(ivHex);
+        messageBit = sjcl.codec.utf8String.toBits(message);
+        prp = new sjcl.cipher.aes(keyBit);
+        try {
+            sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
+            encrypted = sjcl.mode[this.settings.AES_MODE].encrypt(prp, messageBit, ivBit);
+        } catch (_error) {
+            e = _error;
+            throw new CryptoError(e.message, this.settings);
+        }
+        encryptedHex = sjcl.codec.hex.fromBits(encrypted);
+        encryptedBin = helpers.hexTobin(encryptedHex);
+        msg = version + options + encryptionSaltBin + hMacSaltBin + ivBin + encryptedBin;
+        hmacHash = helpers.generateHMACSHA256(this.settings.HMAC_KEY, helpers.binToHex(msg));
+        encryptedMessage = msg + helpers.hexTobin(hmacHash);
+        return helpers.btoa(encryptedMessage);
+    };
 
-  var iv = sjcl.bitArray.bitSlice(message, 18 * 8, 34 * 8);
+    RNCryptor.prototype._extractData = function (data) {
+        var cipher, cipherLength, cipherPos, dataWithoutHMAC, hmac, hmacPos, hmacSalt, hmacSaltPos, items, iv,
+            ivPos, options, optionsPos, salt, saltPos, version, versionPos;
+        versionPos = 0;
+        optionsPos = this.settings.OPTIONS_LENGTH + versionPos;
+        saltPos = optionsPos + versionPos + this.settings.OPTIONS_LENGTH;
+        hmacSaltPos = saltPos + this.settings.ENCRYPTION_SALT_LENGTH;
+        ivPos = hmacSaltPos + this.settings.HMAC_SALT_LENGTH;
+        cipherPos = ivPos + this.settings.IV_LENGTH;
+        cipherLength = data.length - (cipherPos + this.settings.PBKDF2_SIZE);
+        hmacPos = data.length - this.settings.PBKDF2_SIZE;
+        version = data.substr(versionPos, this.settings.VERSION_LENGTH);
+        options = data.substr(optionsPos, this.settings.OPTIONS_LENGTH);
+        salt = data.substr(saltPos, this.settings.ENCRYPTION_SALT_LENGTH);
+        hmacSalt = data.substr(hmacSaltPos, this.settings.HMAC_SALT_LENGTH);
+        iv = data.substr(ivPos, this.settings.IV_LENGTH);
+        cipher = data.substr(cipherPos, cipherLength);
+        hmac = data.substr(hmacPos);
+        dataWithoutHMAC = version + options + salt + hmacSalt + iv + cipher;
+        items = {
+            hmac: helpers.binToHex(hmac),
+            encryptionSalt: helpers.binToHex(salt),
+            ivHex: helpers.binToHex(iv),
+            dataWithoutHMACinHex: helpers.binToHex(dataWithoutHMAC),
+            cipher: cipher
+        };
+        return items;
+    };
 
-  var ciphertext_end = sjcl.bitArray.bitLength(message) - (32 * 8);
+    /**
+     * @param {String} message in base64
+     * @return {String} Decoded utf-8 message
+     * */
+    RNCryptor.prototype.decrypt = function (message) {
+        var cipherBit, data, decrypted, e, hmacHash, items, ivBit, keyBit, prp;
+        data = helpers.atob(helpers.cleanString(message));
+        items = this._extractData(data);
+        hmacHash = helpers.generateHMACSHA256(this.settings.HMAC_KEY, items.dataWithoutHMACinHex);
+        if (hmacHash !== items.hmac) {
+            throw new CryptoError("HMAC " + hmacHash + " mismatch", items);
+        }
+        keyBit = sjcl.codec.hex.toBits(this.settings.KEY);
+        ivBit = sjcl.codec.hex.toBits(items.ivHex);
+        cipherBit = sjcl.codec.base64.toBits(helpers.btoa(items.cipher));
+        prp = new sjcl.cipher.aes(keyBit);
+        try {
+            sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
+            decrypted = sjcl.mode[this.settings.AES_MODE].decrypt(prp, cipherBit, ivBit);
+        } catch (_error) {
+            e = _error;
+            throw new CryptoError(e.message);
+        }
+        return sjcl.codec.utf8String.fromBits(decrypted);
+    };
 
-  var ciphertext = sjcl.bitArray.bitSlice(message, 34 * 8, ciphertext_end);
+    return RNCryptor;
 
-  var hmac = sjcl.bitArray.bitSlice(message, ciphertext_end);
 
-  var expected_hmac = new sjcl.misc.hmac(hmac_key).encrypt(sjcl.bitArray.bitSlice(message, 0, ciphertext_end));
-
-  // .equal is of consistent time
-  if (! sjcl.bitArray.equal(hmac, expected_hmac)) {
-    throw new sjcl.exception.corrupt("HMAC mismatch or bad password.");
-  }
-
-  var aes = new sjcl.cipher.aes(encryption_key);
-  sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
-  var decrypted = sjcl.mode.cbc.decrypt(aes, ciphertext, iv);
-
-  return decrypted;
-}
+})(window);
